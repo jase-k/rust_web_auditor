df35bd9d538c3eab7699c44c94f6f724811e338a
Jace Kraft <jkraft@fame3d.net>
2022-04-22 08:21:19 -0500

    testing os capabilities

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,19 @@
 mod webscraper;
 use webscraper::find_urls::{index_urls, WebScrapingError};
+use std::process::Command;
 
 #[tokio::main]
 async fn main() -> Result<(), WebScrapingError> {
-    index_urls(
-        "https://f3d-shop.forgeflow.io/".to_string(),
-        vec!["https://f3d-shop.forgeflow.io/".to_string()],
-    )
-    .await?;
+    if cfg!(target_os = "windows") {
+        println!("Running configuration for windows");
+    } else if cfg!(target_os = "linux") {
+        println!("Running configuration for linux");
+    }
+
+    // index_urls(
+    //     "https://f3d-shop.forgeflow.io/".to_string(),
+    //     vec!["https://f3d-shop.forgeflow.io/".to_string()],
+    // )
+    // .await?;
     Ok(())
 }
